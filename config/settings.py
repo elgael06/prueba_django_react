ee7c4c9ee5f5d8329443c27ce431bd1eb64d2dfa
Cristian Gael Valenzuela Castro <cgvalenzuelac@concredito.com.mx>
2020-09-11 17:55:36 -0600

    .

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
