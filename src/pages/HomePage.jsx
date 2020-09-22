b57a1569c001825239b0860df368c72550e2f57d
Cristian Gael Valenzuela Castro <cgvalenzuelac@concredito.com.mx>
2020-09-22 13:10:46 -0600

    langs.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,9 @@ import '../styles/home_page.css';
 import foto from  '../assets/image/perfil_1.jpg';
 import info from '../assets/info.json';
 import techs from '../assets/tech.json';
-
+import langs from '../assets/langs.json';
+import { Facebook, GitHub, Mail, PhoneAndroid, WhatsApp } from '@material-ui/icons';
+import { Divider } from '@material-ui/core';
 
 
 const HomePage = () =>{
@@ -18,13 +20,21 @@ const HomePage = () =>{
             <div className='container-personal-info'>
                 <div className='atributos'>
                         {
-                            info.map(item=><section key={item.name}>
+                            info.map(item=><><section key={item.name}>
                                 <span>
                                     <b>{item.name} :</b> 
                                 </span>
                                 <label>{ " \t"+ item.value}</label> 
-                            </section>)
+                            </section>
+                            <Divider />
+                            </>)
                         }
+                        <section className='redes'>
+                            <GitHub onClick={()=>window.open('https://github.com/elgael06')} />
+                            <Facebook onClick={()=>window.open('https://facebook.com/elgael06')} />
+                            <WhatsApp onClick={()=>alert('6674509593')} />
+                            <Mail onClick={()=>alert('elgael06@gmail.com')}  />
+                        </section>
                 </div>
                 <div className='foto-perfil'>
                     <img src={foto} alt='foto-perfil' />
@@ -47,7 +57,16 @@ const HomePage = () =>{
 
             <b className='title-home'>Lenguajes</b>
             <div className='container-personal-info container-stills'>
-
+                {langs.map(item=><section className='item-tech' 
+                    key={item.name}
+                    onClick={()=>window.open(item.uri)}
+                    title={item.name}
+                    style={{
+                        background:`url("${require('../assets/image/lenguajes/'+item.image)}") no-repeat center center`,
+                    backgroundSize:'cover',
+                    backgroundColor:'#dbdbdb'
+                    }}>
+                </section>)}
             </div>
 
             <b className='title-home'>Proyectos</b>
