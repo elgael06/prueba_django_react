013557d031068ce49eb126c4566fe14c1be890a6
Cristian Gael Valenzuela Castro <cgvalenzuelac@concredito.com.mx>
2020-09-22 13:36:23 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
+import image from './assets/image/perfil_1.jpg'
 
 function App() {
   return (
+    <>
+    <head>
+    < link rel="image_src" href={image} />
+    </head>
       <HomePage />
+      </>
   );
 }
 
