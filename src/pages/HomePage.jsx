a082f9868fe7718c6f8be0832b92271e86b4ed85
Cristian Gael Valenzuela Castro <cgvalenzuelac@concredito.com.mx>
2020-09-22 19:47:04 -0600

    starts alerts.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/Layout';
 
 import '../styles/home_page.css';
@@ -6,16 +6,19 @@ import foto from  '../assets/image/perfil_1.jpg';
 import info from '../assets/info.json';
 import techs from '../assets/tech.json';
 import langs from '../assets/langs.json';
-import { Code, Facebook, FolderSpecial, GitHub, LinkedIn, Mail, WhatsApp } from '@material-ui/icons';
-import { Divider } from '@material-ui/core';
+import { BugReport, Build, Close, Code, Facebook, FolderSpecial, GitHub, Info, LinkedIn, Mail, PhoneAndroid, Star, Web, WhatsApp } from '@material-ui/icons';
+import { Button, Chip, Collapse, Divider, IconButton } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
 
 
 const HomePage = () =>{
+    const [email,setEmail] = useState(false);
+    const [cel,setCel] = useState(false);
 
     return(<Layout>
          <div className='container'>
-
-            <h2 className='title-home'><Code /> Cristian Gael Valenzuela Castro.</h2>
+            <h2 className='title-home'><Web /> Cristian Gael Valenzuela Castro.</h2>
+            <hr />
 
             <div className='container-personal-info'>
                 <div className='atributos'>
@@ -32,8 +35,14 @@ const HomePage = () =>{
                         <section className='redes'>
                             <GitHub onClick={()=>window.open('https://github.com/elgael06')} />
                             <Facebook onClick={()=>window.open('https://facebook.com/elgael06')} />
-                            <WhatsApp onClick={()=>alert('6674509593')} />
-                            <Mail onClick={()=>alert('elgael06@gmail.com')}  />
+                            <WhatsApp onClick={()=>{
+                                setCel(true);
+                                setEmail(false);
+                            }} />
+                            <Mail onClick={()=>{
+                                setCel(false);
+                                setEmail(true);
+                            }}/>
                             <LinkedIn onClick={()=>window.open('https://www.linkedin.com/in/cristian-gael-valenzuela-castro-50a755180/')}/>
                             <FolderSpecial />
                         </section>
@@ -54,6 +63,13 @@ const HomePage = () =>{
                 backgroundSize:'cover',
                 backgroundColor:'#dbdbdb'
                 }}>
+                    <Chip 
+                        icon={<Star style={{color:'#ffff0b'}} titleAccess={item.start} />}
+                        label={item.start}
+                        variant='default'
+                        color='secondary'
+                        size='small'
+                    />
                 </section>)}
             </div>
 
@@ -68,14 +84,49 @@ const HomePage = () =>{
                     backgroundSize:'cover',
                     backgroundColor:'#dbdbdb'
                     }}>
+                    <Chip 
+                        icon={<Star style={{color:'#ffff0b'}} titleAccess={item.start} />}
+                        label={item.start}
+                        variant='default'
+                        color='secondary'
+                        size='small'
+                    />
                 </section>)}
             </div>
+            
+            <Button style={{marginBottom:'10px'}} fullWidth color='secondary' startIcon={<BugReport />} variant='contained'>
+                <b>Proyectos</b>
+            </Button>
+            <br />
+            <Button style={{marginBottom:'10px'}} fullWidth color='secondary' startIcon={<Build />} variant='contained'>
+                <b>Experiencia laboral</b>
+            </Button>
 
-            <b className='title-home'>Proyectos</b>
-            <div className='container-personal-info container-stills'>
-
-            </div>
         </div>
+        <Collapse style={{display:'flex',position:'fixed'}} in={email}>
+            <Alert 
+            icon={<Mail fontSize="inherit" />} severity="info"
+            action={
+                <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={()=>setEmail(false)}> <Close/></IconButton>
+            } >elgael06@gmail.com</Alert>
+        </Collapse>
+
+        <Collapse  style={{display:'flex',position:'fixed'}} in={cel}>
+            <Alert 
+            icon={<PhoneAndroid fontSize="inherit" />} severity="warning"
+            action={
+                <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={()=>setCel(false)}> <Close/></IconButton>
+            } >667-4509-593</Alert>
+        </Collapse> 
+
     </Layout>);
 } 
 
